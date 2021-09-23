ff4770607f9ad4f035a4c8381ba633341b3a4f3a
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-09-23 17:17:28 -0400

    Added comments and preload

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+// Import Constants
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
+// Create the window
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, "assets/js/preload.js")
+      }
     })
   
     win.loadFile('index.html')
 }
 
+// Display the Window (Also handels code for MacOS X)
 app.whenReady().then(() => {
     createWindow()
 
@@ -18,6 +25,7 @@ app.whenReady().then(() => {
 
 })
 
+// Close all if on MacOS.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
