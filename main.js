df4cd95ba3e30b6bb8040ea38c6ae8b12f3b4a00
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-09-23 17:21:44 -0400

    Updated/added comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Import Constants
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-// Create the window
+// Create the browser window.
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
@@ -12,20 +12,28 @@ function createWindow () {
       }
     })
   
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
     win.loadFile('index.html')
 }
 
-// Display the Window (Also handels code for MacOS X)
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 
 })
 
-// Close all if on MacOS.
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
